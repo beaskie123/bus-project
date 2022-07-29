915692ac45d3cb3024f631dc1f85cbe0a8832650
bea andrea <petermagalong0421@gmail.com>
2022-07-29 05:41:12 -0700

    hayop

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import bcrypt from 'bcryptjs'
 import User from '../models/userModel.js';
-import { generateToken } from '../utils.js';
+import { generateToken, isAuth } from '../utils.js';
 import expressAsyncHandler from 'express-async-handler'
 
 const userRouter = express.Router();
+
+userRouter.get('/getall', async (req, res) => {
+  const users = await User.find()
+  res.send(users)
+})
 
 userRouter.post(
     '/signin', expressAsyncHandler(async (req, res) => {
@@ -23,5 +28,47 @@ userRouter.post(
         res.status(401).send({message: "Invalid email or password"})
     })
 )
+userRouter.post(
+    '/signup',
+    expressAsyncHandler( async (req, res) => {
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password),
+    })
+    const user = await newUser.save()
+    res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user)
+    })
+    })
+)
+userRouter.put(
+    '/profile',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+      const user = await User.findById(req.user._id);
+      if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if (req.body.password) {
+          user.password = bcrypt.hashSync(req.body.password, 8);
+        }
+  
+        const updatedUser = await user.save();
+        res.send({
+          _id: updatedUser._id,
+          name: updatedUser.name,
+          email: updatedUser.email,
+          isAdmin: updatedUser.isAdmin,
+          token: generateToken(updatedUser),
+        });
+      } else {
+        res.status(404).send({ message: 'User not found' });
+      }
+    })
+  );
 
-export default userRouter
+export default userRouter;
